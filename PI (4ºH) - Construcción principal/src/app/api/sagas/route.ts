104e99a7d37acae89d32f941c20d46cb2f159bce
Paula Martinez Perea <pmarper2003@g.educaand.es>
2026-04-13 20:35:16 +0200

    Segunda entrega

---
+++ PI (4ºH) - Construcción principal/src/app/api/sagas/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/src/lib/prisma";
+
+export async function GET() {
+  try {
+    // AQUÍ ESTÁ EL CAMBIO: prisma.sagas.findMany
+    const sagas = await prisma.sagas.findMany({
+      include: {
+        movies: true, 
+      },
+    });
+    
+    return NextResponse.json(sagas, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Error al obtener el catálogo" }, { status: 500 });
+  }
+}
